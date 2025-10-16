7a2292c30d96279a7da7fdf707e06cdf5793beca
Mukesh <mukeshtopcoder@gmail.com>
2025-10-16 16:13:17 +0530

    SQL_Updates

---
+++ OnO-06AM_IST_Python/Day2.py
@@ -0,0 +1,77 @@
+"""
+Multiple lines comment
+# Single Line Comment
+
+Built-in functions
+print
+
+print('Hello India')
+print("Hello India")
+print('''Hello India''')
+print(""" """)
+
+a = 100
+print('a')
+print(a)
+print('Hello India')
+
+a = 100
+print("Hello a")
+print("Hello ",a)
+
+a = 100
+print("Hello a")
+print("Hello ",a)
+
+# WAP TO ADD TWO NUMBERS.
+
+input function
+input function return text value
+int function
+int (integer) it convert text into numeric value
+
+a = int( input("Enter A Number : ") )
+b = int( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+int do not support float/point/decimal value
+
+float function (supports decimal value)
+a = float( input("Enter A Number : ") )
+b = float( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+
+Build-in Functions
+print , float , int , input
+
+# WAP to calculate the gross salary of an employee
+where HRA is 20% and DA is 30% of his basic salary.
+HINT:- Gross_Salary = Basic_Salary + HRA + DA
+
+bs = float(input("Enter Basic Salary : "))
+hra = bs*0.20
+da  = bs*0.30
+gs = bs+hra+da
+print("Gross Salary :",gs)
+
+
+# WAP to calculate the area of a circle.
+HINT:- Area of a circle = pi*r^2
+
+radius = float(input("Enter Radius : "))
+area = 22/7*radius*radius
+print(area)
+
+
+# WAP to perimeter of a rectangle
+HINT:-  Perimeter = 2*(length+width)
+
+length = float(input("Enter Length : "))
+width = float(input("Enter Width : "))
+peri = 2*(length+width)
+print("Perimeter :",peri)
+
+"""
