4c047817ea5d956baa9c5ae58c0ade13a75c4c84
Mukesh <mukeshtopcoder@gmail.com>
2025-11-22 12:05:08 +0530

    Updated

---
+++ 1130_01PM_Nov_WE_Python/Python_Day3.py
@@ -0,0 +1,28 @@
+# IDLE :- Integrated Development Learning Environment
+"""
+How to create a variable?
+variable_name = value
+Example
+a = 10
+
+aman = 100
+Variable's name rules
+1- Length of the variable name can be anything (undefined)
+amanisadgoodboyinourclassroom = 10
+2- You can use numbers in the variable's name
+aman789 = 100
+3- You can not start name with number
+    789aman = 100  X (it will not work)
+4- You can not use special Symbol
+    besides _ (underscore)
+    aman%kumar  X  , aman_kumar (u can use)
+5- You can not use space
+        aman kumar=10     X (it will not work)
+6- You can start with _ underscore
+    _aman=10 , _789aman = 10
+"""
+
+_25aman = 10
+print(_25aman)
+
+
