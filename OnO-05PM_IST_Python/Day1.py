a8df6522da82c839e48a02bf1c277925ec29c79a
Mukesh <mukeshtopcoder@gmail.com>
2025-12-09 18:10:29 +0530

    Updated

---
+++ OnO-05PM_IST_Python/Day1.py
@@ -0,0 +1,96 @@
+"""
+Computer:- Computer is an electronic machine, which takes
+input and gives desired output and perform arithmetic(+-*/)
+and logical(And Or Not > < etc) operations.
+
+Electricity:
+AC - Alternative (Electric)
+DC - Direct   (Electronic)
+
+Computer's Language
+LLL (Low Level Language)
+1st Gen
+Machine Level / 0 and 1 / Binary Number
+10100100111110101010101101111
+computer:- Fast Execution
+human:- Hard to understand
+
+2nd Gen Assembly Language
+1010101010000001111111 =>   CD MD DIR TREE (Menomic)
+
+
+HLL (High Level Language) / Programming Language
+3rd Gen (Procedural Programming Language)
+C , Cobol , Fortran , Basic , lisp etc
+They are very near to english language
+Translator:- Compiler/Interpreter
+    Compiler -> convert -> HLL -> LLL
+They are not secure
+
+4th Gen (OOP - Object Oriented Programming)
+C++ , Python , Java , R , C# etc
+They are very near to english language
+Translator:- Compiler/Interpreter
+    Compiler -> convert -> HLL -> LLL
+
+5th Gen
+AI
+
+
+Python:- Python is a programming/scripting language
+it is a interpreted language
+
+Python is very near to english language.
+
+ENGLISH
+Alphabets => Words => Senetence => Paragraph
+
+PYTHON
+Alphabets,     Keywords
+Numbers,    => Variables => Instruction => Program
+Symbols        Constant
+
+
+Alphabets:-
+a,b,c,d--z , A,B,C--Z (Case Sensitive)
+    print , Print X
+a => 1100001 => 97
+A => 1000001 => 65
+Aman , aman , AMAN , AMan (all are different)
+
+Numbers:-
+    37 , +763 , -37 , 376.56 , 5/2 etc
+
+Special Symbols:-
+!@#~$%^&*()_+}":?:" etc
+
+----------------------------------------------------------
+Keywords (Reserved Words) (36 keywords)
+if else for while etc
+
+variable/Constant
+    a = 10   ( a is assign to 10 )
+variable , constant
+    variable's value can be change
+    a = 10
+    a = 12
+    a = 15
+
+------------------------------------------------------
+
+a = 10           # instruction
+
+------------------------------------------------------
+
+a = 10
+b = 20            # Program
+c = a+b
+print(c)          #  Output:-   30
+
+a = 568
+b = 464
+c = a+b
+print(c)
+
+"""
+
