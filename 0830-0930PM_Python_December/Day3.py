c87fa08998a0110a3834d2289219713da28add2d
Mukesh <mukeshtopcoder@gmail.com>
2025-12-17 21:35:21 +0530

    Updated

---
+++ 0830-0930PM_Python_December/Day3.py
@@ -0,0 +1,82 @@
+# Single line comment (Not related to the code)
+"""
+Multiline Comments (Not related to the code)
+
+a = 10
+b = 20
+c = a+b
+print(c)
+
+Built-in Functions
+print => print is a function which is use to print a
+statement on the console screen/(Output Screen).
+
+print('Hello India')
+print("Hello World")
+print('''My Name is Mukesh''')
+
+a = 10
+c = 200
+print('a')
+print(a)
+print("c")
+print(c)
+
+
+a = 20
+print("Hello a")        # Print as it is
+print("Hello",a)        # Print a value
+print(f"Hello {a}")     # Print a value
+
+
+input => input is a function which is use to ask a value
+from user.
+
+input always return a text/string value
+
+int => int is a function which is use to convert a value
+into number (if possible) (does not support decimal/point value)
+
+
+a = int( input("Enter A Number : ") )
+b = int( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+
+float => float is a function which is also use to
+convert a value into number (if possible)
+(also support decimal/point value)
+
+a = float( input("Enter A Number : ") )
+b = float( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+
+a = 10.45
+print(a)
+print(type(a))
+
+
+Python is a dynamic programming language
+
+a = 17
+print(a)
+print(type(a))
+
+Built-in Functions
+print , input , int , float
+
+# WAP to calculate gross salary of an employee where
+HRA is 20% and DA is 30% of his basic salary.
+HINT:   Gross_Salary = Basic_Salary + HRA + DA
+
+
+bs = float( input("Enter Basic Salary : ") )
+hra = bs*20/100
+da = bs*30/100
+gs = bs+hra+da
+print("Gross Salary :",gs)
+
+"""
