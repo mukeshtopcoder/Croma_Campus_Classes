8be7c9d2799413c080fbea6646d5e99c353a1fc7
Mukesh <mukeshtopcoder@gmail.com>
2025-12-24 12:00:45 +0530

    Updated

---
+++ 0830-0930PM_Python_December/Day4.py
@@ -0,0 +1,60 @@
+"""
+Python Operators
+a+b    =>    a,b (Operands) ,   +(Operator)
+operator operates on operands
+
+1- Arithmetic Operator
+    + - * / // % **
+
+a = 7
+b = 5
+print( a+b )
+print( a-b )
+print( a*b )
+print( a/b )
+print( a%b )    # % modulus (use to calculate the remainder)
+print( 11%4 )
+print( a//b )   # // floor division/truncation (remove point values from result)
+print( 5//2 )
+print( 5**3 )   # ** Exponential (use to calculate power of a number 5**3 => 5*5*5)
+print( 2**4 )
+
+
+2- Assignment Operator
+    =    += , -= , *= etc (u can use with every arithmetic operator)
+
+a = 10
+a += 1      # a = a+1
+a *= 2      # a = a*2
+print( a )
+
+
+3- Relational Operators  (Return Boolean value True/False)
+    >  <  >=  <=  ==  !=
+
+a = 7
+b = 5
+print( a>b )
+print( a<b )  
+print( a>=b )
+print( a<=b )
+print( a!=b )    #   !=    is not equal to
+print( a==b )    #   ==    is equal to
+
+4- Membership Operator    ( Return Boolean Answer )
+    in , not in    ( Check Existance )
+
+a = "amankumar"
+b = 'aman'
+print( b in a )
+print( "mohan" in a )
+print( "ankum" in a )
+print( "k" in a )
+print( "z" in a )
+print( "ka" in a )
+print( "ka" not in a )
+print( "aman" not in a )
+
+5- Identity Operator
+
+"""
