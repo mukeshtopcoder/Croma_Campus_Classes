de992c65461efc15124a133d1264b49b6a931943
Mukesh <mukeshtopcoder@gmail.com>
2025-12-30 21:16:45 +0530

    Updated

---
+++ 0830-0930PM_Python_December/Day6.py
@@ -0,0 +1,67 @@
+"""
+7- Bitwise Operator  (works on Binary Number)
+and =>  &
+or  =>  |
+
+a = 21
+b = 48
+print( a & b )   #  16
+print( a | b )   #  53
+
+
+a = 29
+b = 42
+print( a & b )   #  8
+print( a | b )   #  63
+
+
+Right Shift Operator <<
+a = 27
+print( a>>1 )   #  13
+print( a>>2 )   #  6
+
+
+Left Shift Operator <<
+a = 9
+print( a<<2 )   # 36
+print( a<<1 )   # 18
+
+
+tile operator (mirror operator)
+a = -5
+print( ~a )  #  4
+a = 19
+print( ~a )  #  -20
+
+XOR  ( ^ )
+# if inputs are same, the result is 0 otherwise 1
+
+a = 23
+b = 44
+print( a^b )   #  59
+
+
+# Guess Output
+
+a = 5
+b = 10
+print(a < b < 20)  #  a<b and b<20
+
+
+x = True
+y = False
+print(x + y * x)  #  1 + 0*1  => 1+0 => 1
+
+
+print(4 ** 0 ** 2)  # 4**0**2  => 4**0 = 4^0=> 1^2 => 1
+
+
+a = 12
+b = 5
+print(a ^ b)
+# 1100
+# 0101
+# 1001   a^b  => 9
+
+"""
+
