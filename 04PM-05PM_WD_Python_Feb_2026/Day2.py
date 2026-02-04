bab528cc287e644c7b3be6b8cf9aa9cad7059290
Mukesh <mukeshtopcoder@gmail.com>
2026-02-04 17:01:38 +0530

    Updated

---
+++ 04PM-05PM_WD_Python_Feb_2026/Day2.py
@@ -0,0 +1,77 @@
+"""
+Multiline Comments
+Built-in Functions
+print
+
+print("Good Morning")
+print("Hello World")
+a = 10
+print("a")
+print(a)
+hello = 100
+print(hello)
+print("Hello India","hello",hello)
+
+
+# Single Line Comments
+
+a = 10
+b = 20
+c = a+b
+print("Addition :",c)
+
+input  ->  it is use to ask value from the user
+input return a text/string value
+
+2+2 =>  4  Addition
+2+2 =>  22 Concatenation
+
+a = input("Enter A Number : ")
+b = input("Enter B Number : ")
+c = a+b
+print("Addition :",c)
+
+
+int (integer =>  numerical value (non-decimal))
+
+a = int( input("Enter A Number : ") )
+b = int( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+
+float ( float   =>  numerical value (decimal))
+
+a = float( input("Enter A Number : ") )
+b = float( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+
+WAP to calculate the gross salary of an employee where
+HRA is 20% and DA is 30% of his basic salary.
+HINT:-  Gross_Salary = Basic_Salary  + HRA + DA 
+
+bs = float( input("Enter Basic Salary : ") )
+hra = bs*20/100
+da = bs*30/100
+gs = bs+hra+da
+print("Gross Salary :",gs)
+
+
+# WAP to calculate the cube of a number by user input
+
+num = int( input("Enter A Number : ") )
+cube = num*num*num
+print( "Cube :",cube )
+
+
+# WAP to calculate the area of a circle
+# AREA =>  3.14 * radius * radius
+
+
+r = int( input("Enter Radius : ") )
+print( "Area ",3.14*r*r )
+
+"""
+
